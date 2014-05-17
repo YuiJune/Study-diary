aa51bb9e617186eda1cf26fb3acfd06240f40106
hongtaozhy <hongtaozhy@qq.com>
2014-05-17 21:57:20 +0800

    Task1 2014-05-18

---
+++ StudyOCFirst/StudyOCFirst/main.m
@@ -0,0 +1,26 @@
+//
+//  main.m
+//  StudyOCFirst
+//
+//  Created by hadn't on 14-5-17.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        int result = 0;
+        int a,b;
+        scanf("%d%d",&a,&b);
+        // 这里开始 Task 1 :通过程序读入的两个数字 输出两个数字的和
+        
+        // 这里结束
+        NSLog(@"%d",result);
+    }
+    return 0;
+}
+
