b3aa1074e615cecb6d7f79ca41b9311fbd1a70c6
YuiJune <locagelou@hotmail.com>
2014-06-05 09:12:12 +0800

    swift 0605

---
+++ swift0605.playground/section-1.swift
@@ -0,0 +1,26 @@
+// Playground - noun: a place where people can play
+
+import Cocoa
+
+/*我的作业
+let apples = 3
+var oranges = 4
+let bananas = 5
+let fruitSummary = "I have \(apples + bananas + oranges) pieces of fruit."
+let fruitsummary = "I have \(bananas) bananas."
+let eatoranges = "I ate an orange."
+var orangesleft = "I have \(oranges - 1) oranges left."
+*/
+
+
+//师父的修正
+let apples = 3      //String
+var oranges = 4     //Int
+let bananas = 5     //String
+var fruitSummary = "I have \(apples + bananas + oranges) pieces of fruit."
+//Int, NSLog
+
+oranges = oranges - 1;
+
+fruitSummary = "I have \(apples + bananas + oranges) pieces of fruit."
+//NSLog
