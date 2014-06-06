8c46dc0b9c206c64c1bb40997ffd1e9e0662c12d
YuiJune <locagelou@hotmail.com>
2014-06-06 09:27:51 +0800

    0606

---
+++ swift0606.playground/section-1.swift
@@ -0,0 +1,37 @@
+// Playground - noun: a place where people can play
+
+import Cocoa
+
+let xuanjing = 1
+var xiaotie = 200
+var chengwu = "I have \(xuanjing) xuanjing and \(xiaotie) xiaotie to built a chengwu"
+
+xiaotie = xiaotie - 2
+let hongtong = 2
+xiaotie = xiaotie + hongtong
+chengwu = "I still have \(xuanjing) xuanjing and \(xiaotie) xiaotie to built a chengwu ."
+
+
+var welcommessage: String
+welcommessage = "hello"
+
+let 玄晶 = 1
+var 小铁 = 200
+小铁 = 198
+//小铁被人黑了两个
+
+println(玄晶)
+//NSLog(@"xuanjing")
+println("方才我喝了杯茶")
+//NSLog(@"方才我喝了杯茶")
+
+println("刚刚我捡了\(xiaotie)块小铁")
+//NSLog(@"刚刚我捡了%d块小铁",xiaotie)
+
+/*注释是一样的
+/*注释里面还可以有注释*/
+最后再来个注释*/
+
+let 红铜 = 10; var 彼岸花 = 10
+let 粗布 = 5.5
+
